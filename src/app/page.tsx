338d5bd940d55715077e2568add16a3af4610699
Trevor Williams <tmw7991@gmail.com>
2023-12-06 12:38:38 -0700

    feat: date picker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import { DatePickerDemo } from "@/components/date-picker";
+
 export default function Home() {
   return (
     <div className="fixed inset-0">
-      <div className="h-full w-full flex items-center justify-center">
-        <h1 className="text-3xl font-semibold">Hello World</h1>
+      <div className="h-full w-full flex pt-20 justify-center">
+        <DatePickerDemo />
       </div>
     </div>
   );
