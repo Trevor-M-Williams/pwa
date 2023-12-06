bb77ac53e1e21ea6d7fb374be2eeecc4a233afb6
Trevor Williams <tmw7991@gmail.com>
2023-12-06 12:29:57 -0700

    fix(home): fix tailwind class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="fixed inset-0">
-      <div className="h-full w-full flex item-center justify-center">
+      <div className="h-full w-full flex items-center justify-center">
         <h1 className="text-3xl font-semibold">Hello World</h1>
       </div>
     </div>
