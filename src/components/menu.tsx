a2e5b48d48117996b299e3453a0626f3b2c8ce3b
Trevor Williams <tmw7991@gmail.com>
2023-12-08 10:16:04 -0700

    style(menu): active icon class

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,15 +1,38 @@
+"use client";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+
 import FormatListBulletedRoundedIcon from "@mui/icons-material/FormatListBulletedRounded";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import NotificationsNoneRoundedIcon from "@mui/icons-material/NotificationsNoneRounded";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 
+type ActiveIcon = "list" | "search" | "notifications" | "settings";
+
 export default function Menu() {
+  const [activeIcon, setActiveIcon] = useState<ActiveIcon>("list");
+
+  const iconClass = "h-8 w-8 text-gray-400";
+  const activeClass = "text-gray-800";
+
   return (
-    <div className="flex justify-between p-8 pt-4  border-top bg-secondary">
-      <FormatListBulletedRoundedIcon className="text-gray-700" />
-      <SearchRoundedIcon className="text-gray-400" />
-      <NotificationsNoneRoundedIcon className="text-gray-400" />
-      <SettingsOutlinedIcon className="text-gray-400" />
+    <div className="flex justify-between p-10 pt-4 border-top bg-secondary">
+      <FormatListBulletedRoundedIcon
+        className={cn(iconClass, activeIcon === "list" && activeClass)}
+        onClick={() => setActiveIcon("list")}
+      />
+      <SearchRoundedIcon
+        className={cn(iconClass, activeIcon === "search" && activeClass)}
+        onClick={() => setActiveIcon("search")}
+      />
+      <NotificationsNoneRoundedIcon
+        className={cn(iconClass, activeIcon === "notifications" && activeClass)}
+        onClick={() => setActiveIcon("notifications")}
+      />
+      <SettingsOutlinedIcon
+        className={cn(iconClass, activeIcon === "settings" && activeClass)}
+        onClick={() => setActiveIcon("settings")}
+      />
     </div>
   );
 }
