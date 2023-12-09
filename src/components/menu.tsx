2dce30c0faaaf276d8fa0194223a2491e83f916e
Trevor Williams <tmw7991@gmail.com>
2023-12-09 07:06:23 -0700

    feat: add navigation - search, notifications, and settings pages

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
 import FormatListBulletedRoundedIcon from "@mui/icons-material/FormatListBulletedRounded";
@@ -8,46 +8,32 @@ import NotificationsNoneRoundedIcon from "@mui/icons-material/NotificationsNoneR
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 
 import { iconClass, largeIconClass, activeIconClass } from "@/lib/styles";
-
-type ActiveIcon = "list" | "search" | "notifications" | "settings";
+import Link from "next/link";
 
 export default function Menu() {
-  const [activeIcon, setActiveIcon] = useState<ActiveIcon>("list");
+  const pathname = usePathname();
+  const icons = [
+    { Icon: FormatListBulletedRoundedIcon, href: "/" },
+    { Icon: SearchRoundedIcon, href: "/search" },
+    { Icon: NotificationsNoneRoundedIcon, href: "/notifications" },
+    { Icon: SettingsOutlinedIcon, href: "/settings" },
+  ];
 
   return (
     <div className="flex justify-between p-10 pt-4 border-top bg-secondary">
-      <FormatListBulletedRoundedIcon
-        className={cn(
-          iconClass,
-          largeIconClass,
-          activeIcon === "list" && activeIconClass
-        )}
-        onClick={() => setActiveIcon("list")}
-      />
-      <SearchRoundedIcon
-        className={cn(
-          iconClass,
-          largeIconClass,
-          activeIcon === "search" && activeIconClass
-        )}
-        onClick={() => setActiveIcon("search")}
-      />
-      <NotificationsNoneRoundedIcon
-        className={cn(
-          iconClass,
-          largeIconClass,
-          activeIcon === "notifications" && activeIconClass
-        )}
-        onClick={() => setActiveIcon("notifications")}
-      />
-      <SettingsOutlinedIcon
-        className={cn(
-          iconClass,
-          largeIconClass,
-          activeIcon === "settings" && activeIconClass
-        )}
-        onClick={() => setActiveIcon("settings")}
-      />
+      {icons.map(({ Icon, href }, i) => (
+        <Link
+          href={href}
+          key={i}
+          className={cn(
+            iconClass,
+            largeIconClass,
+            pathname === href && activeIconClass
+          )}
+        >
+          <Icon />
+        </Link>
+      ))}
     </div>
   );
 }
